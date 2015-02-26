508c53fb6ed7d4b0a441ef09bbc3f1c45d166355
Ben Marini <ben@remind101.com>
2015-02-26 10:04:26 -0800

    Add index on created_at

--- migrations/0001_initial_schema.up.sql
+++ migrations/0001_initial_schema.up.sql
@@ -40,3 +40,4 @@ CREATE UNIQUE INDEX index_apps_on_name ON apps USING btree (name);
 CREATE UNIQUE INDEX index_processes_on_release_id_and_type ON processes USING btree (release_id, "type");
 CREATE UNIQUE INDEX index_slugs_on_image_repo_and_image_id ON slugs USING btree (image_repo, image_id);
 CREATE UNIQUE INDEX index_releases_on_app_id_and_version ON releases USING btree (app_id, version);
+CREATE INDEX index_configs_on_created_at ON configs (created_at);
