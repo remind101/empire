1adabee62b4227a83a687441f3829060234e2864
Ben Marini <ben@remind101.com>
2015-03-04 12:57:37 -0800

    Update to match new http style

--- empire/server/deploys.go
+++ empire/server/deploys.go
@@ -10,6 +10,68 @@ import (
 	"github.com/remind101/empire/empire"
 )
 
+// PostDeploys is a Handler for the POST /v1/deploys endpoint.
+type PostDeploys struct {
+	Empire
+}
+
+// PostDeployForm is the form object that represents the POST body.
+type PostDeployForm struct {
+	Image struct {
+		ID   string `json:"id"`
+		Repo string `json:"repo"`
+	} `json:"image"`
+}
+
+// Serve implements the Handler interface.
+func (h *PostDeploys) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
+	var form PostDeployForm
+
+	if err := Decode(r, &form); err != nil {
+		return err
+	}
+
+	auth, err := parseDockerConfigHeader(r)
+	if err != nil {
+		return err
+	}
+
+	d, err := h.Deploy(empire.Image{
+		Repo: empire.Repo(form.Image.Repo),
+		ID:   form.Image.ID,
+	}, auth)
+	if err != nil {
+		return err
+	}
+
+	w.WriteHeader(201)
+	return Encode(w, d)
+}
+
+func parseDockerConfigHeader(r *http.Request) (*docker.AuthConfigurations, error) {
+	var auth *docker.AuthConfigurations
+	header := r.Header.Get("X-Docker-Config")
+
+	if header != "" {
+		data, err := base64.URLEncoding.DecodeString(header)
+		if err != nil {
+			return nil, err
+		}
+
+		var confs map[string]dockerConfig
+		if err := json.Unmarshal(data, &confs); err != nil {
+			return nil, err
+		}
+
+		auth, err = authConfigs(confs)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return auth, nil
+}
+
 type dockerConfig struct {
 	Auth  string `json:"auth"`
 	Email string `json:"email"`
@@ -38,56 +100,4 @@ func authConfigs(confs map[string]dockerConfig) (*docker.AuthConfigurations, err
 	}
 
 	return c, nil
-}
-
-// PostDeploys is a Handler for the POST /v1/deploys endpoint.
-type PostDeploys struct {
-	Empire
-}
-
-// PostDeployForm is the form object that represents the POST body.
-type PostDeployForm struct {
-	Image struct {
-		ID   string `json:"id"`
-		Repo string `json:"repo"`
-	} `json:"image"`
-}
-
-// Serve implements the Handler interface.
-func (h *PostDeploys) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
-	var form PostDeployForm
-	var auth *docker.AuthConfigurations
-
-	regConfigHeader := req.Header.Get("X-Docker-Config")
-	if regConfigHeader != "" {
-		data, err := base64.URLEncoding.DecodeString(regConfigHeader)
-		if err != nil {
-			return http.StatusInternalServerError, nil, err
-		}
-
-		var confs map[string]dockerConfig
-		if err := json.Unmarshal(data, &confs); err != nil {
-			return http.StatusInternalServerError, nil, err
-		}
-
-		auth, err = authConfigs(confs)
-		if err != nil {
-			return http.StatusInternalServerError, nil, err
-		}
-	}
-
-	if err := Decode(r, &form); err != nil {
-		return err
-	}
-
-	d, err := h.Deploy(empire.Image{
-		Repo: empire.Repo(form.Image.Repo),
-		ID:   form.Image.ID,
-	}, auth)
-	if err != nil {
-		return err
-	}
-
-	w.WriteHeader(201)
-	return Encode(w, d)
 }
