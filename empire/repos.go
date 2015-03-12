cf0e6433f5d568a4270d9510857b25616a6405e2
Eric J. Holmes <eric@ejholmes.net>
2015-03-12 16:25:38 -0700

    Don't need these methods anymore.

--- empire/repos.go
+++ empire/repos.go
@@ -1,9 +1,6 @@
 package empire
 
-import (
-	"database/sql/driver"
-	"strings"
-)
+import "database/sql/driver"
 
 type Repo string
 
@@ -23,25 +20,4 @@ func (r *Repo) Value() (driver.Value, error) {
 	}
 
 	return driver.Value(string(*r)), nil
-}
-
-func (r Repo) Domain() string {
-	domain, _ := r.Split()
-	return domain
-}
-
-func (r Repo) Path() string {
-	_, path := r.Split()
-	return path
-}
-
-// Split splits the repo into the Domain and Path segments.
-func (r Repo) Split() (string, string) {
-	parts := strings.Split(string(r), "/")
-
-	if len(parts) == 2 {
-		return "", strings.Join(parts, "/")
-	}
-
-	return parts[0], strings.Join(parts[1:], "/")
 }
