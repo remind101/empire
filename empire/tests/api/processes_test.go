09eb285b189170ee5b5beaed883f396930de794f
Ben Marini <ben@remind101.com>
2015-04-09 14:54:50 -0700

    Add a test

---
+++ empire/tests/api/processes_test.go
@@ -0,0 +1,32 @@
+package api_test
+
+import (
+	"testing"
+
+	"github.com/bgentry/heroku-go"
+)
+
+func TestProcessesPost(t *testing.T) {
+	c, s := NewTestClient(t)
+	defer s.Close()
+
+	mustDeploy(t, c, DefaultImage)
+	a := true
+
+	d, err := c.DynoCreate("acme-inc", "bash", &heroku.DynoCreateOpts{
+		Attach: &a,
+		Env: &map[string]string{
+			"COLUMNS": "178",
+			"LINES":   "43",
+			"TERM":    "xterm-256color",
+		},
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := *d.AttachURL, "fake://example.com/abc"; got != want {
+		t.Fatalf("AttachURL => %v; want %v", got, want)
+	}
+}
