0cb097f755b30f0f9fca590d9f5cc6e105d0cd80
Ben Marini <ben@remind101.com>
2015-05-06 11:12:01 -0700

    Use keyed fields

--- empire/pkg/service/elb_test.go
+++ empire/pkg/service/elb_test.go
@@ -17,11 +17,11 @@ import (
 func TestECSWithELBManager_Submit_Create(t *testing.T) {
 	h := awsutil.NewHandler([]awsutil.Cycle{
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeSubnets&Filter.1.Name=vpc-id&Filter.1.Value.1=vpc-1&Version=2014-10-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0" encoding="UTF-8"?>
 	<DescribeSubnetsResponse xmlns="http://ec2.amazonaws.com/doc/2015-03-01/">
@@ -44,11 +44,11 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 
 		// No existing load balancers
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeLoadBalancers&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<DescribeLoadBalancersResponse xmlns="http://elasticloadbalancing.amazonaws.com/doc/2012-06-01/">
 	  <DescribeLoadBalancersResult>
@@ -60,12 +60,12 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 
 		// Scale previous service to 0 (in this case there is no previous process)
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.UpdateService",
 				Body:       `{"cluster":"","desiredCount":0,"service":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 400,
 				Body:       `{"__type":"ClientException","message":"Service not found."}`,
 			},
@@ -73,11 +73,11 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 
 		// Create new load balancer
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=CreateLoadBalancer&Listeners.member.1.InstancePort=8080&Listeners.member.1.InstanceProtocol=HTTP&Listeners.member.1.LoadBalancerPort=80&Listeners.member.1.Protocol=HTTP&LoadBalancerName=foo--web&Scheme=internal&SecurityGroups.member.1=internal-sg&Subnets.member.1=subnet-a&Tags.member.1.Key=AppName&Tags.member.1.Value=foo&Tags.member.2.Key=ProcessType&Tags.member.2.Value=web&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0"?>
 <CreateLoadBalancerResponse xmlns="https://route53.amazonaws.com/doc/2013-04-01/">
@@ -88,10 +88,10 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 
 		// Update zone records
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: `/2013-04-01/hostedzonesbyname?dnsname=empire.`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0"?>
 <ListHostedZonesByNameResponse xmlns="https://route53.amazonaws.com/doc/2013-04-01/">
@@ -107,36 +107,36 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: `/2013-04-01/hostedzone/ABCD/rrset`,
 				// Body:       `<ChangeResourceRecordSetsRequest xmlns="https://route53.amazonaws.com/doc/2013-04-01/"><ChangeBatch><Changes><Change><Action>UPSERT</Action><ResourceRecordSet><Name>foo.empire.</Name><ResourceRecords><ResourceRecord><Value>foo--web.us-east-1.elb.amazonaws.com</Value></ResourceRecord></ResourceRecords><TTL>60</TTL><Type>CNAME</Type></ResourceRecordSet></Change></Changes><Comment>Update for empire foo app</Comment></ChangeBatch></ChangeResourceRecordSetsRequest>`,
 				Body: `ignore`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       ``,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.ListServices",
 				Body:       `{"cluster":""}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"serviceArns":[]}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.RegisterTaskDefinition",
 				Body:       `{"containerDefinitions":[{"cpu":128,"command":["acme-inc","web"],"environment":[{"name":"USER","value":"foo"}],"essential":true,"image":"remind101/acme-inc:latest","memory":128,"name":"web","portMappings":[{"containerPort":8080,"hostPort":8080}]}],"family":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       "",
 			},
@@ -144,24 +144,24 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 
 		// We try to update first, if that fails with service not found, we try to create.
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.UpdateService",
 				Body:       `{"cluster":"","desiredCount":0,"service":"foo--web","taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 400,
 				Body:       `{"__type":"ClientException","message":"Service not found."}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.CreateService",
 				Body:       `{"cluster":"","desiredCount":0,"loadBalancers":[{"containerName":"web","containerPort":8080,"loadBalancerName":"foo--web"}],"role":"ecsServiceRole","serviceName":"foo--web","taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       "",
 			},
@@ -180,11 +180,11 @@ func TestECSWithELBManager_Submit_Create(t *testing.T) {
 func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 	h := awsutil.NewHandler([]awsutil.Cycle{
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeSubnets&Filter.1.Name=vpc-id&Filter.1.Value.1=vpc-1&Version=2014-10-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0" encoding="UTF-8"?>
 	<DescribeSubnetsResponse xmlns="http://ec2.amazonaws.com/doc/2015-03-01/">
@@ -207,11 +207,11 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Existing load balancer, publicly exposed
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeLoadBalancers&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<DescribeLoadBalancersResponse xmlns="http://elasticloadbalancing.amazonaws.com/doc/2012-06-01/">
 	  <DescribeLoadBalancersResult>
@@ -249,11 +249,11 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Tags for previous load balancer
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeTags&LoadBalancerNames.member.1=foo--web&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<DescribeTagsResponse xmlns="http://elasticloadbalancing.amazonaws.com/doc/2012-06-01/">
 	  <DescribeTagsResult>
@@ -279,12 +279,12 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Scale previous service to 0
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.UpdateService",
 				Body:       `{"cluster":"","desiredCount":0,"service":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"Service":{}}`,
 			},
@@ -292,12 +292,12 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Delete the service
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.DeleteService",
 				Body:       `{"cluster":"","service":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"Service":{}}`,
 			},
@@ -305,11 +305,11 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Delete old load balancer
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DeleteLoadBalancer&LoadBalancerName=foo--web&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{}`,
 			},
@@ -317,11 +317,11 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Create new load balancer
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=CreateLoadBalancer&Listeners.member.1.InstancePort=8080&Listeners.member.1.InstanceProtocol=HTTP&Listeners.member.1.LoadBalancerPort=80&Listeners.member.1.Protocol=HTTP&LoadBalancerName=foo--web&Scheme=internal&SecurityGroups.member.1=internal-sg&Subnets.member.1=subnet-a&Tags.member.1.Key=AppName&Tags.member.1.Value=foo&Tags.member.2.Key=ProcessType&Tags.member.2.Value=web&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0"?>
 <CreateLoadBalancerResponse xmlns="https://route53.amazonaws.com/doc/2013-04-01/">
@@ -332,10 +332,10 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// Update zone records
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: `/2013-04-01/hostedzonesbyname?dnsname=empire.`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0"?>
 <ListHostedZonesByNameResponse xmlns="https://route53.amazonaws.com/doc/2013-04-01/">
@@ -351,36 +351,36 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: `/2013-04-01/hostedzone/ABCD/rrset`,
 				// Body:       `<ChangeResourceRecordSetsRequest xmlns="https://route53.amazonaws.com/doc/2013-04-01/"><ChangeBatch><Changes><Change><Action>UPSERT</Action><ResourceRecordSet><Name>foo.empire.</Name><ResourceRecords><ResourceRecord><Value>foo--web.us-east-1.elb.amazonaws.com</Value></ResourceRecord></ResourceRecords><TTL>60</TTL><Type>CNAME</Type></ResourceRecordSet></Change></Changes><Comment>Update for empire foo app</Comment></ChangeBatch></ChangeResourceRecordSetsRequest>`,
 				Body: `ignore`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       ``,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.ListServices",
 				Body:       `{"cluster":""}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"serviceArns":[]}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.RegisterTaskDefinition",
 				Body:       `{"containerDefinitions":[{"cpu":128,"command":["acme-inc","web"],"environment":[{"name":"USER","value":"foo"}],"essential":true,"image":"remind101/acme-inc:latest","memory":128,"name":"web","portMappings":[{"containerPort":8080,"hostPort":8080}]}],"family":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       "",
 			},
@@ -388,24 +388,24 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 
 		// We try to update first, if that fails with service not found, we try to create.
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.UpdateService",
 				Body:       `{"cluster":"","desiredCount":0,"service":"foo--web","taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 400,
 				Body:       `{"__type":"ClientException","message":"Service not found."}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.CreateService",
 				Body:       `{"cluster":"","desiredCount":0,"loadBalancers":[{"containerName":"web","containerPort":8080,"loadBalancerName":"foo--web"}],"role":"ecsServiceRole","serviceName":"foo--web","taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       "",
 			},
@@ -424,11 +424,11 @@ func TestECSWithELBManager_Submit_Recreate(t *testing.T) {
 func TestECSWithELBManager_Submit_No_Recreate(t *testing.T) {
 	h := awsutil.NewHandler([]awsutil.Cycle{
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeSubnets&Filter.1.Name=vpc-id&Filter.1.Value.1=vpc-1&Version=2014-10-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<?xml version="1.0" encoding="UTF-8"?>
 <DescribeSubnetsResponse xmlns="http://ec2.amazonaws.com/doc/2015-03-01/">
@@ -451,11 +451,11 @@ func TestECSWithELBManager_Submit_No_Recreate(t *testing.T) {
 
 		// Existing load balancer.
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeLoadBalancers&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<DescribeLoadBalancersResponse xmlns="http://elasticloadbalancing.amazonaws.com/doc/2012-06-01/">
   <DescribeLoadBalancersResult>
@@ -493,11 +493,11 @@ func TestECSWithELBManager_Submit_No_Recreate(t *testing.T) {
 
 		// Tags for previous load balancer
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Body:       `Action=DescribeTags&LoadBalancerNames.member.1=foo--web&Version=2012-06-01`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body: `<DescribeTagsResponse xmlns="http://elasticloadbalancing.amazonaws.com/doc/2012-06-01/">
   <DescribeTagsResult>
@@ -523,48 +523,48 @@ func TestECSWithELBManager_Submit_No_Recreate(t *testing.T) {
 
 		// Previous service exists
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.ListServices",
 				Body:       `{"cluster":""}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"serviceArns":["arn:aws:ecs:us-east-1a:249285743859:service/foo--web"]}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.DescribeServices",
 				Body:       `{"cluster":"","services":["arn:aws:ecs:us-east-1a:249285743859:service/foo--web"]}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"services":[{"desiredCount":1,"taskDefinition":"foo--web"}]}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.DescribeTaskDefinition",
 				Body:       `{"taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"taskDefinition":{"containerDefinitions":[{"cpu":128,"command":["acme-inc","web"],"environment":[{"name":"USER","value":"foo"}],"essential":true,"image":"remind101/acme-inc:latest","memory":128,"name":"web"}]}}`,
 			},
 		},
 
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.RegisterTaskDefinition",
 				Body:       `{"containerDefinitions":[{"cpu":128,"command":["acme-inc","web"],"environment":[{"name":"USER","value":"foo"}],"essential":true,"image":"remind101/acme-inc:latest","memory":128,"name":"web","portMappings":[{"containerPort":8080,"hostPort":8080}]}],"family":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       "",
 			},
@@ -572,12 +572,12 @@ func TestECSWithELBManager_Submit_No_Recreate(t *testing.T) {
 
 		// We try to update first, if that fails with service not found, we try to create.
 		awsutil.Cycle{
-			awsutil.Request{
+			Request: awsutil.Request{
 				RequestURI: "/",
 				Operation:  "AmazonEC2ContainerServiceV20141113.UpdateService",
 				Body:       `{"cluster":"","desiredCount":1,"service":"foo--web","taskDefinition":"foo--web"}`,
 			},
-			awsutil.Response{
+			Response: awsutil.Response{
 				StatusCode: 200,
 				Body:       `{"service":{"desiredCount":1,"taskDefinition":"foo--web"}}`,
 			},
