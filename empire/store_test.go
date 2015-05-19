5ae8828d869d3ba08ef09a94562f85dbdfa5140f
Eric J. Holmes <eric@ejholmes.net>
2015-05-18 17:29:57 -0700

    Test framework for scopes.

--- empire/store_test.go
+++ empire/store_test.go
@@ -1,10 +1,13 @@
 package empire
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestComposedScope(t *testing.T) {
@@ -39,4 +42,34 @@ func MockScope(called chan struct{}) Scope {
 		close(called)
 		return db
 	})
+}
+
+// scopeTest is a struct for testing scopes.
+type scopeTest struct {
+	scope Scope
+	sql   string
+	vars  []interface{}
+}
+
+func assertScopeSql(t testing.TB, scope Scope, sql string, vars ...interface{}) {
+	db, err := gorm.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ds := scope.Scope(&db).NewScope(nil)
+	got := strings.TrimSpace(ds.CombinedConditionSql())
+
+	t.Logf("SQL: %s", got)
+	t.Logf("Variables: %v", ds.SqlVars)
+
+	if got != sql {
+		t.Fatalf("SQL => %v; want %v", got, sql)
+	}
+
+	if got, want := ds.SqlVars, vars; !reflect.DeepEqual(got, want) {
+		if len(got) > 0 && len(want) > 0 {
+			t.Fatalf("Vars => %v; want %v", got, want)
+		}
+	}
 }
