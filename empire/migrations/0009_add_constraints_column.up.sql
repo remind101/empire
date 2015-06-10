7860b8f160cb266953d0a900783cc2eed4b43eb5
Eric J. Holmes <eric@ejholmes.net>
2015-06-10 10:05:57 -0700

    Currently things are 1GB.

--- empire/migrations/0009_add_constraints_column.up.sql
+++ empire/migrations/0009_add_constraints_column.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE processes ADD COLUMN cpu_share int;
 ALTER TABLE processes ADD COLUMN memory int;
 
-UPDATE processes SET cpu_share = 256, memory = 536870912;
+UPDATE processes SET cpu_share = 256, memory = 1073741824;
