1f07b62bec7e1c8b5f3f00dbea2df477ead29193
Eric J. Holmes <eric@ejholmes.net>
2015-06-26 13:45:12 -0700

    Cleanup common deploy command.

--- empire/tests/cli/rollback_test.go
+++ empire/tests/cli/rollback_test.go
@@ -4,26 +4,8 @@ import "testing"
 
 func TestRollback(t *testing.T) {
 	run(t, []Command{
-		{
-			"deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2",
-			`Pulling repository remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc, endpoint: https://registry-1.docker.io/v1/
-345c7524bc96: Pulling dependent layers
-a1dd7097a8e8: Download complete
-Status: Image is up to date for remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
-Status: Created new release v1 for acme-inc`,
-		},
-		{
-			"deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2",
-			`Pulling repository remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc, endpoint: https://registry-1.docker.io/v1/
-345c7524bc96: Pulling dependent layers
-a1dd7097a8e8: Download complete
-Status: Image is up to date for remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
-Status: Created new release v2 for acme-inc`,
-		},
+		DeployCommand("latest", "v1"),
+		DeployCommand("latest", "v2"),
 		{
 			"set FOO=bar -a acme-inc",
 			"Set env vars and restarted acme-inc.",
@@ -34,8 +16,8 @@ Status: Created new release v2 for acme-inc`,
 		},
 		{
 			"releases -a acme-inc",
-			`v1    Dec 31 17:01  Deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
-v2    Dec 31 17:01  Deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
+			`v1    Dec 31 17:01  Deploy remind101/acme-inc:latest
+v2    Dec 31 17:01  Deploy remind101/acme-inc:latest
 v3    Dec 31 17:01  Set FOO config vars
 v4    Dec 31 17:01  Rollback to v1`,
 		},
--- empire/tests/cli/run_test.go
+++ empire/tests/cli/run_test.go
@@ -4,16 +4,7 @@ import "testing"
 
 func TestRunDetached(t *testing.T) {
 	run(t, []Command{
-		{
-			"deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2",
-			`Pulling repository remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc, endpoint: https://registry-1.docker.io/v1/
-345c7524bc96: Pulling dependent layers
-a1dd7097a8e8: Download complete
-Status: Image is up to date for remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
-Status: Created new release v1 for acme-inc`,
-		},
+		DeployCommand("latest", "v1"),
 		{
 			"run -d migration -a acme-inc",
 			"Ran `migration` on acme-inc as run, detached.",
@@ -23,16 +14,7 @@ Status: Created new release v1 for acme-inc`,
 
 func TestRunAttached(t *testing.T) {
 	run(t, []Command{
-		{
-			"deploy remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2",
-			`Pulling repository remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc
-345c7524bc96: Pulling image (9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2) from remind101/acme-inc, endpoint: https://registry-1.docker.io/v1/
-345c7524bc96: Pulling dependent layers
-a1dd7097a8e8: Download complete
-Status: Image is up to date for remind101/acme-inc:9ea71ea5abe676f117b2c969a6ea3c1be8ed4098d2118b1fd9ea5a5e59aa24f2
-Status: Created new release v1 for acme-inc`,
-		},
+		DeployCommand("latest", "v1"),
 		{
 			"run migration -a acme-inc",
 			"Fake output for `migration` on acme-inc",
