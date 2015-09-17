7da824cb3d418531ffa6ce6d953d123c9aaa59e7
Eric J. Holmes <eric@ejholmes.net>
2015-09-17 11:55:20 -0700

    Add stub implementations for kubernetes, cloudformation and docker.

---
+++ scheduler/cloudformation/cloudformation.go
@@ -0,0 +1,5 @@
+// Package cloudformation implements the Scheduler interface for ECS by using
+// CloudFormation to provision and update resources.
+//
+// TODO: Implement this.
+package cloudformation
---
+++ scheduler/docker/docker.go
@@ -0,0 +1,6 @@
+// Package docker implements the Scheduler interface backed by the Docker API.
+// This implementation is not recommended for production use, but can be used in
+// development for testing.
+//
+// TODO: Implement this.
+package docker
