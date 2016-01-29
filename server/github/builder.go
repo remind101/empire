b5e93bf777bf2b1f806306afd983f12cccd13f9c
Eric J. Holmes <eric@ejholmes.net>
2016-01-29 11:16:30 +0700

    Add a conveyor image builder implementation.

--- server/github/builder.go
+++ server/github/builder.go
@@ -2,9 +2,12 @@ package github
 
 import (
 	"bytes"
+	"io"
+	"io/ioutil"
 	"text/template"
 
 	"github.com/ejholmes/hookshot/events"
+	"github.com/remind101/conveyor/client/conveyor"
 	"github.com/remind101/empire/pkg/image"
 	"golang.org/x/net/context"
 )
@@ -33,4 +36,41 @@ func ImageFromTemplate(t *template.Template) ImageBuilder {
 
 		return image.Decode(buf.String())
 	})
+}
+
+// conveyorClient mocks the interface to the Conveyor API.
+type conveyorClient interface {
+	Build(io.Writer, conveyor.BuildCreateOpts) (*conveyor.Artifact, error)
+}
+
+// ConveyorImageBuilder provides an ImageBuilder implementation that
+// integrations with the Conveyor (https://github.com/remind101/conveyor) Docker
+// build system. If enabled, Empire will check if an Artifact in Conveyor exists
+// for the git commit, and will trigger Conveyor to build it if it doesn't
+// exist.
+type ConveyorImageBuilder struct {
+	client conveyorClient
+}
+
+// NewConveyorImageBuilder returns a new ConveyorImageBuilder implementation
+// that uses the given client.
+func NewConveyorImageBuilder(c *conveyor.Service) *ConveyorImageBuilder {
+	return &ConveyorImageBuilder{
+		client: c,
+	}
+}
+
+func (c *ConveyorImageBuilder) BuildImage(ctx context.Context, event events.Deployment) (image.Image, error) {
+	w := ioutil.Discard // TODO: Allow ImageBuilder implementations to write to the deployment logs?
+
+	a, err := c.client.Build(w, conveyor.BuildCreateOpts{
+		Repository: event.Repository.FullName,
+		Branch:     "", // TODO,
+		Sha:        event.Deployment.Sha,
+	})
+	if err != nil {
+		return image.Image{}, err
+	}
+
+	return image.Decode(a.Image)
 }
