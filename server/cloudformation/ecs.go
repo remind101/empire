f46aeeb1ccf05832c11347623735f5db6d44a449
Eric J. Holmes <eric@ejholmes.net>
2016-05-24 06:39:52 +0700

    Cleanup.

--- server/cloudformation/ecs.go
+++ server/cloudformation/ecs.go
@@ -15,15 +15,19 @@ type ecsClient interface {
 	UpdateService(*ecs.UpdateServiceInput) (*ecs.UpdateServiceOutput, error)
 }
 
+type LoadBalancer struct {
+	ContainerName    *string
+	ContainerPort    *IntValue
+	LoadBalancerName *string
+}
+
+// ECSServiceProperties represents the properties for the Custom::ECSService
+// resource.
 type ECSServiceProperties struct {
-	ServiceName   *string
-	Cluster       *string
-	DesiredCount  *IntValue
-	LoadBalancers []struct {
-		ContainerName    *string
-		ContainerPort    *IntValue
-		LoadBalancerName *string
-	}
+	ServiceName    *string
+	Cluster        *string
+	DesiredCount   *IntValue
+	LoadBalancers  []LoadBalancer
 	Role           *string
 	TaskDefinition *string
 }
@@ -67,6 +71,9 @@ func (p *ECSServiceResource) Provision(req Request) (string, interface{}, error)
 		return *resp.Service.ServiceArn, nil, nil
 	case Delete:
 		id := req.PhysicalResourceId
+
+		// We have to scale the service down to 0, before we're able to
+		// destroy it.
 		_, err := p.ecs.UpdateService(&ecs.UpdateServiceInput{
 			Service:      aws.String(id),
 			Cluster:      properties.Cluster,
@@ -85,30 +92,42 @@ func (p *ECSServiceResource) Provision(req Request) (string, interface{}, error)
 	case Update:
 		id := req.PhysicalResourceId
 
-		if !reflect.DeepEqual(properties.Cluster, oldProperties.Cluster) {
-			return id, nil, errors.New("cannot update cluster")
-		}
-
-		if !reflect.DeepEqual(properties.Role, oldProperties.Role) {
-			return id, nil, errors.New("cannot update role")
-		}
-
-		if !reflect.DeepEqual(properties.ServiceName, oldProperties.ServiceName) {
-			return id, nil, errors.New("cannot update service name")
-		}
-
-		if !reflect.DeepEqual(properties.LoadBalancers, oldProperties.LoadBalancers) {
-			return id, nil, errors.New("cannot update load balancers")
+		if err := canUpdateService(properties, oldProperties); err != nil {
+			return id, nil, err
 		}
 
 		_, err := p.ecs.UpdateService(&ecs.UpdateServiceInput{
-			Service:      aws.String(id),
-			Cluster:      properties.Cluster,
-			DesiredCount: properties.DesiredCount.Value(),
+			Service:        aws.String(id),
+			Cluster:        properties.Cluster,
+			DesiredCount:   properties.DesiredCount.Value(),
+			TaskDefinition: properties.TaskDefinition,
 		})
 
 		return id, nil, err
 	default:
 		return "", nil, fmt.Errorf("%s is not supported", req.RequestType)
 	}
+}
+
+// We currently only support updating the task definition and desired count.
+func canUpdateService(new, old *ECSServiceProperties) error {
+	eq := reflect.DeepEqual
+
+	if !eq(new.Cluster, old.Cluster) {
+		return errors.New("cannot update cluster")
+	}
+
+	if !eq(new.Role, old.Role) {
+		return errors.New("cannot update role")
+	}
+
+	if !eq(new.ServiceName, old.ServiceName) {
+		return errors.New("cannot update service name")
+	}
+
+	if !eq(new.LoadBalancers, old.LoadBalancers) {
+		return errors.New("cannot update load balancers")
+	}
+
+	return nil
 }
--- server/cloudformation/ecs_test.go
+++ server/cloudformation/ecs_test.go
@@ -46,26 +46,64 @@ func TestECSServiceResource_Update(t *testing.T) {
 	}
 
 	e.On("UpdateService", &ecs.UpdateServiceInput{
-		Service:      aws.String("arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web"),
-		Cluster:      aws.String("cluster"),
-		DesiredCount: aws.Int64(2),
+		Service:        aws.String("arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web"),
+		Cluster:        aws.String("cluster"),
+		DesiredCount:   aws.Int64(2),
+		TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:2"),
 	}).Return(&ecs.UpdateServiceOutput{}, nil)
 
 	id, data, err := p.Provision(Request{
 		RequestType:        Update,
 		PhysicalResourceId: "arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web",
 		ResourceProperties: &ECSServiceProperties{
-			Cluster:      aws.String("cluster"),
-			ServiceName:  aws.String("acme-inc-web"),
-			DesiredCount: intValue(2),
+			Cluster:        aws.String("cluster"),
+			ServiceName:    aws.String("acme-inc-web"),
+			DesiredCount:   intValue(2),
+			TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:2"),
 		},
 		OldResourceProperties: &ECSServiceProperties{
-			Cluster:      aws.String("cluster"),
-			ServiceName:  aws.String("acme-inc-web"),
-			DesiredCount: intValue(1),
+			Cluster:        aws.String("cluster"),
+			ServiceName:    aws.String("acme-inc-web"),
+			DesiredCount:   intValue(1),
+			TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:1"),
 		},
 	})
 	assert.NoError(t, err)
+	assert.Equal(t, "arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web", id)
+	assert.Nil(t, data)
+}
+
+func TestECSServiceResource_Update_Cluster(t *testing.T) {
+	e := new(mockECS)
+	p := &ECSServiceResource{
+		ecs: e,
+	}
+
+	e.On("UpdateService", &ecs.UpdateServiceInput{
+		Service:        aws.String("arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web"),
+		Cluster:        aws.String("cluster"),
+		DesiredCount:   aws.Int64(2),
+		TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:2"),
+	}).Return(&ecs.UpdateServiceOutput{}, nil)
+
+	id, data, err := p.Provision(Request{
+		RequestType:        Update,
+		PhysicalResourceId: "arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web",
+		ResourceProperties: &ECSServiceProperties{
+			Cluster:        aws.String("clusterB"),
+			ServiceName:    aws.String("acme-inc-web"),
+			DesiredCount:   intValue(2),
+			TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:2"),
+		},
+		OldResourceProperties: &ECSServiceProperties{
+			Cluster:        aws.String("clusterA"),
+			ServiceName:    aws.String("acme-inc-web"),
+			DesiredCount:   intValue(1),
+			TaskDefinition: aws.String("arn:aws:ecs:us-east-1:012345678910:task-definition/acme-inc:1"),
+		},
+	})
+	assert.Error(t, err)
+	assert.EqualError(t, err, "cannot update cluster")
 	assert.Equal(t, "arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web", id)
 	assert.Nil(t, data)
 }
@@ -104,6 +142,62 @@ func TestECSServiceResource_Delete(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "arn:aws:ecs:us-east-1:012345678901:service/acme-inc-web", id)
 	assert.Nil(t, data)
+}
+
+func TestCanUpdateService(t *testing.T) {
+	tests := []struct {
+		new, old ECSServiceProperties
+		out      bool
+	}{
+		{
+			ECSServiceProperties{Cluster: aws.String("cluster"), TaskDefinition: aws.String("td:2"), DesiredCount: intValue(1)},
+			ECSServiceProperties{Cluster: aws.String("cluster"), TaskDefinition: aws.String("td:1"), DesiredCount: intValue(0)},
+			true,
+		},
+
+		{
+			ECSServiceProperties{LoadBalancers: []LoadBalancer{{ContainerName: aws.String("web"), ContainerPort: intValue(8080), LoadBalancerName: aws.String("elb")}}},
+			ECSServiceProperties{LoadBalancers: []LoadBalancer{{ContainerName: aws.String("web"), ContainerPort: intValue(8080), LoadBalancerName: aws.String("elb")}}},
+			true,
+		},
+
+		// Can't change clusters.
+		{
+			ECSServiceProperties{Cluster: aws.String("clusterB")},
+			ECSServiceProperties{Cluster: aws.String("clusterA")},
+			false,
+		},
+
+		// Can't change name.
+		{
+			ECSServiceProperties{ServiceName: aws.String("acme-inc-B")},
+			ECSServiceProperties{ServiceName: aws.String("acme-inc-A")},
+			false,
+		},
+
+		// Can't change role.
+		{
+			ECSServiceProperties{Role: aws.String("roleB")},
+			ECSServiceProperties{Role: aws.String("roleA")},
+			false,
+		},
+
+		// Can't change load balancers
+		{
+			ECSServiceProperties{LoadBalancers: []LoadBalancer{{ContainerName: aws.String("web"), ContainerPort: intValue(8080), LoadBalancerName: aws.String("elbB")}}},
+			ECSServiceProperties{LoadBalancers: []LoadBalancer{{ContainerName: aws.String("web"), ContainerPort: intValue(8080), LoadBalancerName: aws.String("elbA")}}},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		out := canUpdateService(&tt.new, &tt.old)
+		if tt.out {
+			assert.Nil(t, out)
+		} else {
+			assert.Error(t, out)
+		}
+	}
 }
 
 type mockECS struct {
