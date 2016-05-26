f78c61d712ea8ddfeaef8c9d1352569869c357ca
Michael Hahn <mwhahn@gmail.com>
2016-05-26 15:59:30 -0700

    Use new AppProperties struct

--- server/cloudformation/apps.go
+++ server/cloudformation/apps.go
@@ -9,17 +9,32 @@ import (
 	"github.com/remind101/empire"
 )
 
+// empireClient mocks the Empire interface we use.
+type empireClient interface {
+	AppsFind(empire.AppsQuery) (*empire.App, error)
+	Create(context.Context, empire.CreateOpts) (*empire.App, error)
+	Destroy(context.Context, empire.DestroyOpts) error
+}
+
+// AppProperties represents the properties for the Custom::EmpirApp
+type AppProperties struct {
+	Name string
+}
+
+// AppResource is a Provisioner that will manage an Empire application
 type AppResource struct {
-	empire *empire.Empire
+	empire empireClient
 }
 
 func (p *AppResource) Provision(req Request) (id string, data interface{}, err error) {
 	ctx := context.Background()
 	user := NewUser()
 
+	properties := req.ResourceProperties.(*AppProperties)
+
 	switch req.RequestType {
 	case Create:
-		name := req.ResourceProperties["Name"].(string)
+		name := properties.Name
 		app, err := p.empire.AppsFind(empire.AppsQuery{
 			Name: &name,
 		})
@@ -28,8 +43,9 @@ func (p *AppResource) Provision(req Request) (id string, data interface{}, err e
 		}
 
 		app, err = p.empire.Create(ctx, empire.CreateOpts{
-			User: user,
-			Name: name,
+			User:    user,
+			Name:    name,
+			Message: "Creating app via Cloudformation",
 		})
 		if err != nil {
 			return "", nil, err
@@ -45,11 +61,14 @@ func (p *AppResource) Provision(req Request) (id string, data interface{}, err e
 			return id, nil, err
 		}
 
-		// TODO: Handle error
-		_ = p.empire.Destroy(ctx, empire.DestroyOpts{
-			User: user,
-			App:  app,
+		err = p.empire.Destroy(ctx, empire.DestroyOpts{
+			User:    user,
+			App:     app,
+			Message: "Destroying app via Cloudformation",
 		})
+		if err != nil {
+			return id, nil, err
+		}
 
 		return id, nil, nil
 	case Update:
--- server/cloudformation/apps_test.go
+++ server/cloudformation/apps_test.go
@@ -1,286 +1,87 @@
 package cloudformation
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"testing"
 
-	"golang.org/x/net/context"
-
+	"github.com/jinzhu/gorm"
 	"github.com/remind101/empire"
-	"github.com/remind101/empire/empiretest"
-	"github.com/remind101/empire/pkg/image"
-	"github.com/remind101/empire/scheduler"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAppResourceProvision_Create(t *testing.T) {
-	e := empiretest.NewEmpire(t)
-	s := new(mockScheduler)
-	e.Scheduler = s
+	e := new(mockEmpire)
+	user := NewUser()
 
-	user := &empire.User{Name: "ejholmes"}
-
-	// Create an app
-	app, err := e.Create(context.Background(), empire.CreateOpts{
-		User: user,
+	app := empire.App{
+		ID:   "1234",
 		Name: "acme-inc",
+	}
+	e.On("AppsFind", empire.AppsQuery{
+		Name: &app.Name,
+	}).Once().Return(&empire.App{}, gorm.RecordNotFound)
+	e.On("Create", empire.CreateOpts{
+		User:    user,
+		Name:    app.Name,
+		Message: "Creating app via Cloudformation",
+	}).Once().Return(&app, nil)
+
+	resource := &AppResource{empire: e}
+	id, _, err := resource.Provision(Request{
+		RequestType: Create,
+		ResourceProperties: &AppProperties{
+			Name: app.Name,
+		},
 	})
-	assert.NoError(t, err)
-
-	// Add some environment variables to it.
-	prod := "production"
-	_, err = e.Set(context.Background(), empire.SetOpts{
-		User: user,
-		App:  app,
-		Vars: empire.Vars{
-			"RAILS_ENV": &prod,
-		},
-	})
-	assert.NoError(t, err)
-
-	// Deploy a new image to the app.
-	img := image.Image{Repository: "remind101/acme-inc"}
-	s.On("Submit", &scheduler.App{
-		ID:   app.ID,
-		Name: "acme-inc",
-		Env: map[string]string{
-			"EMPIRE_APPID":      app.ID,
-			"EMPIRE_APPNAME":    "acme-inc",
-			"EMPIRE_RELEASE":    "v1",
-			"EMPIRE_CREATED_AT": "2015-01-01T01:01:01Z",
-			"RAILS_ENV":         "production",
-		},
-		Labels: map[string]string{
-			"empire.app.name":    "acme-inc",
-			"empire.app.id":      app.ID,
-			"empire.app.release": "v1",
-		},
-		Processes: []*scheduler.Process{
-			{
-				Type:    "web",
-				Image:   img,
-				Command: []string{"./bin/web"},
-				Exposure: &scheduler.Exposure{
-					Type: &scheduler.HTTPExposure{},
-				},
-				Instances:   1,
-				MemoryLimit: 536870912,
-				CPUShares:   256,
-				Nproc:       256,
-				Env: map[string]string{
-					"EMPIRE_PROCESS": "web",
-					"SOURCE":         "acme-inc.web.v1",
-				},
-				Labels: map[string]string{
-					"empire.app.process": "web",
-				},
-			},
-		},
-	}).Once().Return(nil)
-
-	_, err = e.Deploy(context.Background(), empire.DeploymentsCreateOpts{
-		App:    app,
-		User:   user,
-		Output: ioutil.Discard,
-		Image:  img,
-	})
-	assert.NoError(t, err)
-
-	// Add cloudformation environment
-	s.On("Submit", &scheduler.App{
-		ID:   app.ID,
-		Name: "acme-inc",
-		Env: map[string]string{
-			"EMPIRE_APPID":      app.ID,
-			"EMPIRE_APPNAME":    "acme-inc",
-			"EMPIRE_RELEASE":    "v2",
-			"EMPIRE_CREATED_AT": "2015-01-01T01:01:01Z",
-			"RAILS_ENV":         "production",
-			"COMPUTERS":         "woo",
-		},
-		Labels: map[string]string{
-			"empire.app.name":    "acme-inc",
-			"empire.app.id":      app.ID,
-			"empire.app.release": "v2",
-		},
-		Processes: []*scheduler.Process{
-			{
-				Type:    "web",
-				Image:   img,
-				Command: []string{"./bin/web"},
-				Exposure: &scheduler.Exposure{
-					Type: &scheduler.HTTPExposure{},
-				},
-				Instances:   1,
-				MemoryLimit: 536870912,
-				CPUShares:   256,
-				Nproc:       256,
-				Env: map[string]string{
-					"EMPIRE_PROCESS": "web",
-					"SOURCE":         "acme-inc.web.v2",
-				},
-				Labels: map[string]string{
-					"empire.app.process": "web",
-				},
-			},
-		},
-	}).Once().Return(nil)
-
-	resource := &EnvironmentResource{empire: e}
-	var req Request
-	err = json.Unmarshal([]byte(fmt.Sprintf(`{"RequestType": "Create", "ResourceProperties": {"AppId": "%s", "Variables": [{"Name": "COMPUTERS", "Value": "woo"}]}}`, app.ID)), &req)
-	assert.NoError(t, err)
-
-	id, _, err := resource.Provision(req)
 	assert.NoError(t, err)
 	assert.Equal(t, id, app.ID)
-	s.AssertExpectations(t)
+	e.AssertExpectations(t)
 }
 
 func TestAppResource_Update(t *testing.T) {
-	e := empiretest.NewEmpire(t)
-	s := new(mockScheduler)
-	e.Scheduler = s
+	e := new(mockEmpire)
 
-	user := &empire.User{Name: "ejholmes"}
-
-	// Create an app
-	app, err := e.Create(context.Background(), empire.CreateOpts{
-		User: user,
+	app := empire.App{
+		ID:   "1234",
 		Name: "acme-inc",
-	})
-	assert.NoError(t, err)
-
-	// Deploy a new image to the app.
-	img := image.Image{Repository: "remind101/acme-inc"}
-	s.On("Submit", &scheduler.App{
-		ID:   app.ID,
-		Name: "acme-inc",
-		Env: map[string]string{
-			"EMPIRE_APPID":      app.ID,
-			"EMPIRE_APPNAME":    "acme-inc",
-			"EMPIRE_RELEASE":    "v1",
-			"EMPIRE_CREATED_AT": "2015-01-01T01:01:01Z",
-		},
-		Labels: map[string]string{
-			"empire.app.name":    "acme-inc",
-			"empire.app.id":      app.ID,
-			"empire.app.release": "v1",
-		},
-		Processes: []*scheduler.Process{
-			{
-				Type:    "web",
-				Image:   img,
-				Command: []string{"./bin/web"},
-				Exposure: &scheduler.Exposure{
-					Type: &scheduler.HTTPExposure{},
-				},
-				Instances:   1,
-				MemoryLimit: 536870912,
-				CPUShares:   256,
-				Nproc:       256,
-				Env: map[string]string{
-					"EMPIRE_PROCESS": "web",
-					"SOURCE":         "acme-inc.web.v1",
-				},
-				Labels: map[string]string{
-					"empire.app.process": "web",
-				},
-			},
-		},
-	}).Once().Return(nil)
-
-	_, err = e.Deploy(context.Background(), empire.DeploymentsCreateOpts{
-		App:    app,
-		User:   user,
-		Output: ioutil.Discard,
-		Image:  img,
-	})
-	assert.NoError(t, err)
+	}
+	e.On("AppsFind", empire.AppsQuery{
+		ID: &app.ID,
+	}).Once().Return(&app, nil)
 
 	resource := &AppResource{empire: e}
-	var req Request
-	err = json.Unmarshal([]byte(fmt.Sprintf(`{"RequestType": "Update", "PhysicalResourceId": "%s", "ResourceProperties": {"Name": "renamed"}, "OldResourceProperties": {"Name": "acme-inc"}}`, app.ID)), &req)
-	assert.NoError(t, err)
-
-	_, _, err = resource.Provision(req)
+	_, _, err := resource.Provision(Request{
+		RequestType:        Update,
+		PhysicalResourceId: app.ID,
+		ResourceProperties: &AppProperties{},
+	})
 	assert.EqualError(t, err, "Updates are not supported")
-	s.AssertExpectations(t)
+	e.AssertExpectations(t)
 }
 
 func TestAppResourceProvision_Delete(t *testing.T) {
-	e := empiretest.NewEmpire(t)
-	s := new(mockScheduler)
-	e.Scheduler = s
+	e := new(mockEmpire)
+	user := NewUser()
 
-	user := &empire.User{Name: "ejholmes"}
-
-	// Create an app
-	app, err := e.Create(context.Background(), empire.CreateOpts{
-		User: user,
+	app := empire.App{
+		ID:   "1234",
 		Name: "acme-inc",
-	})
-	assert.NoError(t, err)
-
-	// Deploy a new image to the app.
-	img := image.Image{Repository: "remind101/acme-inc"}
-	s.On("Submit", &scheduler.App{
-		ID:   app.ID,
-		Name: "acme-inc",
-		Env: map[string]string{
-			"EMPIRE_APPID":      app.ID,
-			"EMPIRE_APPNAME":    "acme-inc",
-			"EMPIRE_RELEASE":    "v1",
-			"EMPIRE_CREATED_AT": "2015-01-01T01:01:01Z",
-		},
-		Labels: map[string]string{
-			"empire.app.name":    "acme-inc",
-			"empire.app.id":      app.ID,
-			"empire.app.release": "v1",
-		},
-		Processes: []*scheduler.Process{
-			{
-				Type:    "web",
-				Image:   img,
-				Command: []string{"./bin/web"},
-				Exposure: &scheduler.Exposure{
-					Type: &scheduler.HTTPExposure{},
-				},
-				Instances:   1,
-				MemoryLimit: 536870912,
-				CPUShares:   256,
-				Nproc:       256,
-				Env: map[string]string{
-					"EMPIRE_PROCESS": "web",
-					"SOURCE":         "acme-inc.web.v1",
-				},
-				Labels: map[string]string{
-					"empire.app.process": "web",
-				},
-			},
-		},
+	}
+	e.On("AppsFind", empire.AppsQuery{
+		ID: &app.ID,
+	}).Once().Return(&app, nil)
+	e.On("Destroy", empire.DestroyOpts{
+		User:    user,
+		App:     &app,
+		Message: "Destroying app via Cloudformation",
 	}).Once().Return(nil)
 
-	_, err = e.Deploy(context.Background(), empire.DeploymentsCreateOpts{
-		App:    app,
-		User:   user,
-		Output: ioutil.Discard,
-		Image:  img,
+	resource := &AppResource{empire: e}
+	id, _, err := resource.Provision(Request{
+		RequestType:        Delete,
+		PhysicalResourceId: app.ID,
+		ResourceProperties: &AppProperties{},
 	})
 	assert.NoError(t, err)
-
-	// Remove the app
-	s.On("Remove", app.ID).Once().Return(nil)
-
-	resource := &AppResource{empire: e}
-	var req Request
-	err = json.Unmarshal([]byte(fmt.Sprintf(`{"RequestType": "Delete", "PhysicalResourceId": "%s", "ResourceProperties": {"Name": "acme-inc"}}`, app.ID)), &req)
-	assert.NoError(t, err)
-
-	id, _, err := resource.Provision(req)
-	assert.NoError(t, err)
 	assert.Equal(t, id, app.ID)
-	s.AssertExpectations(t)
+	e.AssertExpectations(t)
 }
