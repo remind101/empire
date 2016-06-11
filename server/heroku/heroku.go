08b8e2a596ba8e1fd06171854d8add4535a74916
Eric J. Holmes <eric@ejholmes.net>
2016-06-11 09:44:03 +0700

    Report errors from heroku api.

--- server/heroku/heroku.go
+++ server/heroku/heroku.go
@@ -13,6 +13,7 @@ import (
 	"github.com/remind101/empire/pkg/heroku"
 	"github.com/remind101/empire/server/auth"
 	"github.com/remind101/pkg/httpx"
+	"github.com/remind101/pkg/reporter"
 )
 
 // The Accept header that controls the api version. See
@@ -73,16 +74,13 @@ func New(e *empire.Empire, authenticator auth.Authenticator) httpx.Handler {
 	// Logs
 	r.Handle("/apps/{app}/log-sessions", &PostLogs{e}).Methods("POST") // hk log
 
-	errorHandler := func(err error, w http.ResponseWriter, r *http.Request) {
-		Error(w, err, http.StatusInternalServerError)
-	}
-
 	api := Authenticate(r, authenticator)
 
 	return httpx.HandlerFunc(func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		err := api.ServeHTTPContext(ctx, w, r)
 		if err != nil {
-			errorHandler(err, w, r)
+			Error(w, err, http.StatusInternalServerError)
+			reporter.Report(ctx, err)
 		}
 		return nil
 	})
