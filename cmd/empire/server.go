8ce9e45cf0d13366cb7febac82c982828e4436d1
Michael Hahn <mwhahn@gmail.com>
2016-07-28 09:59:53 -0700

    Fix merge conflict

--- cmd/empire/server.go
+++ cmd/empire/server.go
@@ -81,7 +81,7 @@ func newServer(c *Context, e *empire.Empire) http.Handler {
 	return middleware.Handler(c, h)
 }
 
-func newCloudFormationCustomResourceProvisioner(db *empire.Empire, c *Context) *cloudformation.CustomResourceProvisioner {
+func newCloudFormationCustomResourceProvisioner(e *empire.Empire, c *Context) *cloudformation.CustomResourceProvisioner {
 	p := cloudformation.NewCustomResourceProvisioner(e, c)
 	p.QueueURL = c.String(FlagCustomResourcesQueue)
 	p.Context = c
