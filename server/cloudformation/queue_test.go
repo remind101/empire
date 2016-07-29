20abd8197c8c2939e11edfe5594f339cc6c3b97a
Eric J. Holmes <eric@ejholmes.net>
2016-07-29 09:52:30 -0700

    Fix extension of message visibility.

--- server/cloudformation/queue_test.go
+++ server/cloudformation/queue_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
@@ -40,6 +41,35 @@ func TestSQSDispatcher_Handle(t *testing.T) {
 		ReceiptHandle: aws.String("MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+CwLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0="),
 	})
 	assert.NoError(t, err)
+
+	s.AssertExpectations(t)
+}
+
+func TestSQSDispatcher_Handle_ChangeMessageVisibility(t *testing.T) {
+	s := new(mockSQSClient)
+	q := &SQSDispatcher{
+		VisibilityHeartbeat: defaultVisibilityHeartbeat,
+		QueueURL:            "https://sqs.amazonaws.com",
+		sqs:                 s,
+		after: func(d time.Duration) <-chan time.Time {
+			return nil
+		},
+	}
+
+	awsErr := awserr.New("AccessDenied", "Stack with id acme-inc does not exist", errors.New(""))
+	s.On("ChangeMessageVisibility", &sqs.ChangeMessageVisibilityInput{
+		QueueUrl:          aws.String("https://sqs.amazonaws.com"),
+		VisibilityTimeout: aws.Int64(60),
+		ReceiptHandle:     aws.String("MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+CwLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0="),
+	}).Return(&sqs.ChangeMessageVisibilityOutput{}, awsErr).Once()
+
+	handle := func(ctx context.Context, message *sqs.Message) error {
+		return nil
+	}
+	err := q.handle(ctx, handle, &sqs.Message{
+		ReceiptHandle: aws.String("MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+CwLj1FjgXUv1uSj1gUPAWV66FU/WeR4mq2OKpEGYWbnLmpRCJVAyeMjeU5ZBdtcQ+QEauMZc8ZRv37sIW2iJKq3M9MFx1YvV11A2x/KSbkJ0="),
+	})
+	assert.Equal(t, awsErr, err)
 
 	s.AssertExpectations(t)
 }
