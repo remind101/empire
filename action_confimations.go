e00a7e44c6afe2dccdf994d53b133aee08652571
Eric J. Holmes <eric@ejholmes.net>
2016-10-12 17:04:17 -0700

    User Go template to determine duo username

--- action_confimations.go
+++ action_confimations.go
@@ -1,9 +1,11 @@
 package empire
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/url"
+	"text/template"
 
 	"github.com/remind101/empire/pkg/duo"
 )
@@ -19,6 +21,10 @@ type ActionConfirmer interface {
 // DuoActionConfirmer is an ActionConfirmer that will send the user a Duo push
 // notification to confirm the action before continuing.
 type DuoActionConfirmer struct {
+	// A template that will be used to determine the users Duo username. The
+	// template will be executed with a User object.
+	UsernameTemplate *template.Template
+
 	duo *duo.Client
 }
 
@@ -32,8 +38,13 @@ func NewDuoActionConfirmer(key, secret, apiHost string) *DuoActionConfirmer {
 }
 
 func (c *DuoActionConfirmer) Confirm(ctx context.Context, user *User, action string, resource string, params map[string]string) (bool, error) {
+	username, err := c.username(user)
+	if err != nil {
+		return false, err
+	}
+
 	q := url.Values{}
-	q.Add("username", user.Name)
+	q.Add("username", username)
 	q.Add("factor", "push")
 	q.Add("device", "auto")
 	q.Add("type", action)
@@ -45,4 +56,20 @@ func (c *DuoActionConfirmer) Confirm(ctx context.Context, user *User, action str
 	}
 
 	return resp.Response.Result == "allow", nil
+}
+
+var defaultUsernameTemplate = template.Must(template.New("username").Parse(`{{.Name}}`))
+
+func (c *DuoActionConfirmer) username(user *User) (string, error) {
+	t := c.UsernameTemplate
+	if t == nil {
+		t = defaultUsernameTemplate
+	}
+
+	b := new(bytes.Buffer)
+	if err := t.Execute(b, user); err != nil {
+		return "", fmt.Errorf("duo confirmation: error finding username: %v", err)
+	}
+
+	return b.String(), nil
 }
